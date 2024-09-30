6a1da4d24efb029875e5b691ff080fc833d35f98
Fatima <fatimaissa24al@gmail.com>
2024-09-30 15:42:53 +0300

    new random

--- client/src/components/RandomDuck/RandomDuck.js
+++ client/src/components/RandomDuck/RandomDuck.js
@@ -3,20 +3,64 @@ import styles from './RandomDuck.module.css';
 import { DuckContext } from '../../context/DuckContext';
 import FirstButton from '../common/FirstButton/FirstButton';
 
+
 const RandomDuck = () => {
-  const { duck, getRandomDuck } = useContext(DuckContext);
-
-  if (!duck) return null;
-
   return (
     <div className={styles.container}>
-      <FirstButton onClick={getRandomDuck}>hello </FirstButton>
-      {duck && (
-          <div className={styles.duck}>
-            <h2 className={styles.duckName}>{duck.name}</h2>
-            {duck.imageUrl && <img src={duck.imageUrl} alt={duck.name} className={styles.img} />}
-          </div>
-        )}
+      {/* Dashboard structure */}
+      <div className={styles.dashboard}>
+        <h1>Dashboard</h1>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Thumbnail</th>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Description</th>
+              <th>Tags</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Example row */}
+            <tr>
+              <td>
+                <img
+                  src="https://www.akhbarelbalad.net/images/cms-image-000006140.jpg"
+                  alt="القدس"
+                  className={styles.thumbnail}
+                />
+              </td>
+              <td>القدس</td>
+              <td>1/1/1925</td>
+              <td>مكان مقدس</td>
+              <td>#عباده</td>
+              <td>
+                <button className={styles.editBtn}>Edit</button>
+                <button className={styles.deleteBtn}>Delete</button>
+              </td>
+            </tr>
+            {/* Another row */}
+            <tr>
+              <td>
+                <img
+                  src="https://felesteen.news/thumb/w920/uploads/images/2022/09/8adTp.jpg"
+                  alt="حيفا"
+                  className={styles.thumbnail}
+                />
+              </td>
+              <td>حيفا</td>
+              <td>09/09/1956</td>
+              <td>مكان جميل جدا</td>
+              <td>#سياحه</td>
+              <td>
+                <button className={styles.editBtn}>Edit</button>
+                <button className={styles.deleteBtn}>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
